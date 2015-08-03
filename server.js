bc65ffe5b85c42aa4e5ba44f092ff128e336b5de
Humberto Vieira de Castro <humbertovieira.castro@gmail.com>
2015-08-03 18:18:31 -0300

    Quarto

--- server.js
+++ server.js
@@ -7,39 +7,40 @@ var fs = require('fs');
 
 /* Inicializa o express */
 var app = express();
+Parse.initialize("lLT4FaRKGEP7O2CFIq7p9g99VOR4qsCH6Seh6BnD", "DQLZI1H8gpmdL2xds9WGQ5WyUQVB8acVHr9fnoaJ");
 
 
 app.get('/historia/:id', function(req, res, next){
 	console.log("ID do cara: " + req.params.id);
-
-	Parse.initialize("lLT4FaRKGEP7O2CFIq7p9g99VOR4qsCH6Seh6BnD", "DQLZI1H8gpmdL2xds9WGQ5WyUQVB8acVHr9fnoaJ");
+	
+	var objectID = req.params.id;
 
 	var objHistoria = Parse.Object.extend("Historia");
 	var query = new Parse.Query(objHistoria);
-	query.equalTo("objectId", "qH8lXfA9Bv");
 	
 	var html = "aa";
 	res.write("");
 	
-	query.first({
+	query.get(objectID, {
   		success: function(object) {
-    		console.log("ID Historia: " + object.id);
+  			console.log(object);
+    		//console.log("ID Historia: " + object.id);
     		console.log("Descrição: " + object["attributes"].descricao);
-			console.log("Titulo: " + object["attributes"].titulo);
-    		console.log("ID do autor: " + object["attributes"].autor.id);
+			//console.log("Titulo: " + object["attributes"].titulo);
+    		//console.log("ID do autor: " + object["attributes"].autor.id);
 
-    		console.log("Foto URL: " + object["attributes"].foto._url);
-    		console.log("Audio URL: " + object["attributes"].audio._url);
+    		//console.log("Foto URL: " + object["attributes"].foto._url);
+    		//console.log("Audio URL: " + object["attributes"].audio._url);
 
-
-    		var titulo = object["attributes"].titulo
-    		var descricao = object["attributes"].descricao
+    		
+    		var titulo = object["attributes"].titulo;
+    		var descricao = object["attributes"].descricao;
     		var audioURL = object["attributes"].audio._url;
     		var fotoURL = object["attributes"].foto._url;
 
    			html = "<html>" + 
 							"<head>"+
-								"<title> Audio cool! </title>" +
+								"<title> Share Audio </title>" +
 							"</head>" +
 							"<body>" +
 								"<h2>" + titulo + "</h2>" +
@@ -52,10 +53,11 @@ app.get('/historia/:id', function(req, res, next){
     		
         	res.write(html, function(err){
         		res.end();
+        		next();
         	});
   		},
   		error: function(error) {
-	  		console.log("NAO PEGOU NADA" + error.message);
+	  		console.log("NAO PEGOU NADA");
 
   		}
 
